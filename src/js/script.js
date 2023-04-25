a74479441ac80d114dcac959d5b066597c1c4f11
Alexander M <alexmal74@gmail.com>
2023-04-25 15:39:00 +0500

    Запрет прокрутки документа при открытом модальном окне

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,29 @@
 
 let modal = document.getElementById("formModal");
 
+
+let bodyOverflow = document.body.style.overflow;
+
+function disableScroll() {
+	let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+	document.body.style.overflow = 'hidden';
+	document.body.style.top = '-' + scrollPosition + 'px';
+}
+
+function enableScroll() {
+	document.body.style.overflow = bodyOverflow;
+	document.body.style.top = '0';
+}
+
 function openModal() {
 	modal.style.display = "block";
+	disableScroll();
 }
 
 function closeModal() {
 	modal.style.display = "none";
+	enableScroll();
 }
-
 
 function submitForm() {
 	event.preventDefault();
